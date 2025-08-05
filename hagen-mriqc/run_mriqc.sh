78efc6e8a2c9d023f1f8e6500c3fd2b77657a97b
mckenziephagen <mphagen@uw.edu>
2025-08-05 10:48:34 -0700

    fix run_mriqc

--- hagen-mriqc/run_mriqc.sh
+++ hagen-mriqc/run_mriqc.sh
@@ -1,1 +1,18 @@
-run_mriqc.sh
+#!/bin/bash
+
+#read from CLI
+sub_id=$1
+
+# TODO: define needed directories
+bids_dir="/Users/kenziepaige/Projects/2025-Neurohackademy/PNC_BIDS"
+output_dir="/Users/kenziepaige/Projects/2025-Neurohackademy/derivatives/mriqc"
+
+
+docker run -v $bids_dir:/bids_dir \
+	-v $output_dir:/output_dir \
+	nipreps/mriqc:latest \
+	/bids_dir \
+	/output_dir \
+	participant \
+	--participant_label $sub_id \
+
